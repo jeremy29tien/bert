7b819900f69e1ac5ea9d33d55c8c0fd3742eee78
Jeremy Tien <jtien@berkeley.edu>
2022-11-30 16:43:24 -0800

    organize code

--- check_embeddings.py
+++ check_embeddings.py
@@ -9,8 +9,9 @@ with open('tmp/output.jsonl') as embedding_file:
     for data_string in data_list:
         result = json.loads(data_string)
         data.append(result)
-        print(f"result: {result}")
-        print(isinstance(result, dict))
+        # print(f"result: {result}")
+        # print(isinstance(result, dict))
 
-    print(data[0]['features'])
+    # The beginning of each string has a '[CLS]' token appended. There may also be '[SEP]' tokens.
+    print(data[0]['features'][1]['layers'][0]['values'])
 
