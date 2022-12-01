6700e9dd72fe1f98f3055676d44ec7fe7e642f9a
Jeremy Tien <jeremy29tien@gmail.com>
2022-11-30 17:57:28 -0800

    changes to tester

--- check_embeddings.py
+++ check_embeddings.py
@@ -13,5 +13,16 @@ with open('tmp/output.jsonl') as embedding_file:
         # print(isinstance(result, dict))
 
     # The beginning of each string has a '[CLS]' token appended. There may also be '[SEP]' tokens.
-    print(data[0]['features'][1]['layers'][0]['values'])
+    # print(data[0]['features'][1]['layers'][0]['values'])
+    first_line = data[0]['features']
+    king_tok_layers = first_line[1]['layers']
+    king_tok_embedding = king_tok_layers[0]['values']  # We are taking the embeddings from the last Transformer layer.
+
+    queen_tok_embedding = data[1]['features'][1]['layers'][0]['values']
+    prince_tok_embedding = data[2]['features'][1]['layers'][0]['values']
+
+    print("king_tok_embedding:", king_tok_embedding)
+    print("queen_tok_embedding:", queen_tok_embedding)
+    print("prince_tok_embedding:", prince_tok_embedding)
+
 
