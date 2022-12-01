07fb1746998c9e197186db044e90cbec806a6fc1
Jeremy Tien <jeremy29tien@gmail.com>
2022-11-30 18:29:47 -0800

    changes to tester

--- check_embeddings.py
+++ check_embeddings.py
@@ -22,16 +22,19 @@ with open('tmp/output.jsonl') as embedding_file:
     queen_tok_embedding = data[1]['features'][1]['layers'][0]['values']
     prince_tok_embedding = data[2]['features'][1]['layers'][0]['values']
     chair_tok_embedding = data[3]['features'][1]['layers'][0]['values']
+    guitar_tok_embedding = data[4]['features'][1]['layers'][0]['values']
 
     king_tok_embedding = np.asarray(king_tok_embedding)
     queen_tok_embedding = np.asarray(queen_tok_embedding)
     prince_tok_embedding = np.asarray(prince_tok_embedding)
     chair_tok_embedding = np.asarray(chair_tok_embedding)
+    guitar_tok_embedding = np.asarray(guitar_tok_embedding)
 
     print("king_tok_embedding:", king_tok_embedding)
     print("queen_tok_embedding:", queen_tok_embedding)
     print("prince_tok_embedding:", prince_tok_embedding)
     print("chair_tok_embedding:", chair_tok_embedding)
+    print("guitar_tok_embedding:", guitar_tok_embedding)
 
     print("king - queen = ", np.linalg.norm(king_tok_embedding - queen_tok_embedding))
     print("king - prince = ", np.linalg.norm(king_tok_embedding - prince_tok_embedding))
@@ -39,5 +42,9 @@ with open('tmp/output.jsonl') as embedding_file:
     print("king - chair = ", np.linalg.norm(king_tok_embedding - chair_tok_embedding))
     print("queen - chair = ", np.linalg.norm(queen_tok_embedding - chair_tok_embedding))
     print("prince - chair = ", np.linalg.norm(prince_tok_embedding - chair_tok_embedding))
+    print("king - guitar = ", np.linalg.norm(king_tok_embedding - guitar_tok_embedding))
+    print("queen - guitar = ", np.linalg.norm(queen_tok_embedding - guitar_tok_embedding))
+    print("prince - guitar = ", np.linalg.norm(prince_tok_embedding - guitar_tok_embedding))
+    print("chair - guitar = ", np.linalg.norm(chair_tok_embedding - guitar_tok_embedding))
 
 
