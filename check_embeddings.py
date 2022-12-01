f576dba9abef7cf68fd1aa1f5f0487168a13846f
Jeremy Tien <jeremy29tien@gmail.com>
2022-11-30 16:36:13 -0800

    add tester file

--- check_embeddings.py
+++ check_embeddings.py
@@ -5,5 +5,12 @@ with open('tmp/output.jsonl') as embedding_file:
     data_list = list(embedding_file)
     # data = json.load(embedding_file)
 
-    # Print the data of dictionary
-    print(data_list[0])
+    data = []
+    for data_string in data_list:
+        result = json.loads(data_string)
+        data.append(result)
+        print(f"result: {result}")
+        print(isinstance(result, dict))
+
+    print(data[0]['features'])
+
